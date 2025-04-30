68762a33e561abe25f6b26885b5432c295890122
opu183059 <opuctg440@gmail.com>
2025-04-30 07:47:03 +0600

    initialize MotoServe backend with TypeScript, Express, and Prisma

---
+++ README.md
@@ -0,0 +1,9 @@
+# MotoServe Server
+
+## Installation and Setup
+
+1. Clone this repository: `git clone <repository_url>`
+2. Install dependencies: `npm install`
+3. Set up the environment variables by creating a `.env` file
+4. Run the database migrations: `npx prisma migrate dev`
+5. Start the server: `npm run dev`
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import httpStatus from "http-status";
+
+const app: Application = express();
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "MotoServe server...",
+  });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
+
+export default app;
